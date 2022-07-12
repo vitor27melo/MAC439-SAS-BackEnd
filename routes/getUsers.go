9104618a67c024061d074cf29131c59ae1035ea0
Rodrigo Vidotti <rodrigo.vidotti4@gmail.com>
2022-07-12 17:48:04 -0300

    refactor: :recycle: switching to CheckError and returning results

--- routes/getUsers.go
+++ routes/getUsers.go
@@ -1,28 +1,27 @@
 package routes
 
 import (
+	"backend/tools"
 	"context"
-	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"net/http"
-	"time"
 )
 
 var coll *mongo.Collection
 
 func GetUsers(c echo.Context) error {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://mac439:Atila_Iamarino@mac439.fsau9s1.mongodb.net/?retryWrites=true&w=majority"))
-	if err != nil {
-		panic(err)
-	}
+	tools.CheckError(err)
+
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
-	if err != nil {
-		panic(err)
-	}
+	tools.CheckError(err)
+
 	defer client.Disconnect(ctx)
 
 	coll = client.Database("test").Collection("usuario")
@@ -30,16 +29,11 @@ func GetUsers(c echo.Context) error {
 	findOptions := options.Find()
 	findOptions.SetLimit(5)
 	cursor, err := coll.Find(context.TODO(), bson.D{{}}, findOptions)
-	if err != nil {
-		panic(err)
-	}
+	tools.CheckError(err)
 
 	var results []bson.M
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-	for _, result := range results {
-		fmt.Println(result)
-	}
-	return c.JSON(http.StatusOK, "")
+	err = cursor.All(context.TODO(), &results)
+	tools.CheckError(err)
+
+	return c.JSON(http.StatusOK, results)
 }
--- server.go
+++ server.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"backend/routes"
-	"github.com/labstack/echo/v4"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func healthz(c echo.Context) error {
