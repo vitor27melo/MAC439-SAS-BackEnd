a8e483710bf751cd765c3c02b81f5af2bc0ea31f
TurriJP <joaopedroturri@gmail.com>
2022-07-19 11:57:02 -0300

    Fixing linter errors

--- routes/calculateRisk.go
+++ routes/calculateRisk.go
@@ -2,9 +2,12 @@ package routes
 
 import (
 	"backend/configs"
+	"backend/tools"
 
 	"github.com/labstack/echo/v4"
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+
+	"fmt"
 )
 
 func CalculateRisk(c echo.Context, id string) float64 {
@@ -19,6 +22,17 @@ func CalculateRisk(c echo.Context, id string) float64 {
 	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeRead})
 	defer session.Close()
 
+	greeting, err := session.ReadTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
+
+		result, err := transaction.Run(
+			"MATCH (n:Dia) RETURN n.data as data LIMIT 25",
+			map[string]interface{}{})
+		tools.CheckError(err)
+
+		return result, nil
+	})
+
+	fmt.Print(greeting, c, id, err)
 	/*
 		stmtQuery := `
 		SELECT
